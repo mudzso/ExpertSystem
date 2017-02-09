7249bcc9765bd3d0778b69de68899f61d7b86f0d
mudzso <mudzso@gmail.com>
2017-02-09 17:50:26 +0100

    finished FactRepository.java

--- src/FactRepository.java
+++ src/FactRepository.java
@@ -5,32 +5,41 @@ import java.util.Map;
  */
 public class FactRepository {
 
-    private Map<String,Question> facts = new LinkedHashMap<>();
+    private Iterator iterator;
+    private Map<String,Fact> facts = new LinkedHashMap<>();
 
+    public FactRepository() {
+
+        class FactIterator implements Iterator {
+
+            int index = 0;
+
+
+            public boolean hasNext() {
+
+                return index < facts.size();
+            }
+
+            public Object next() {
+
+                Fact result = facts.get(index);
+                index++;
+                return result;
+            }
+        }
+        this.iterator = new FactIterator();
+    }
+    
     public Iterator getIterator() {
-        return null;
+
+        return iterator;
     }
 
-    public void addFact(Fact fact) {
+    public void addFact(String id,Fact fact) {
 
+        facts.put(id,fact);
     }
 
 
-    class FactIterator {
 
-        int index = 0;
-
-
-        public boolean hasNext() {
-
-            return index < facts.size();
-        }
-
-        public Object next() {
-
-            Question result = facts.get(index);
-            index++;
-            return result;
-        }
-    }
 }
